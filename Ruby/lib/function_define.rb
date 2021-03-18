a517549ddab5b5036bc1f7febaac95050fde5e51
Takahiro Iwasaki <takahiro@example.com>
2021-03-18 22:56:41 +0900

    add Ruby program(2 files)

---
+++ Ruby/lib/function_define.rb
@@ -0,0 +1,8 @@
+#３の倍数ならtrue、それ以外はfalse
+def multiple_of_three?(n)
+  puts n % 3 == 0
+end
+
+multiple_of_three?(4)
+multiple_of_three?(5)
+multiple_of_three?(6)
---
+++ Ruby/lib/function_define_2.rb
@@ -0,0 +1,7 @@
+def reverse_upcase!(s)
+  s.reverse!.upcase!
+end
+
+s = 'ruby'
+reverse_upcase!(s)
+puts s
