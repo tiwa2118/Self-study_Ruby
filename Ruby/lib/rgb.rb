0e38faac903e041b6a5bf16930277e8d6774c87f
Takahiro Iwasaki <takahiro@example.com>
2021-04-07 23:18:00 +0900

    modify rgb

--- Ruby/lib/rgb.rb
+++ Ruby/lib/rgb.rb
@@ -1,14 +1,11 @@
+# 10進数→１６進数変換
 def to_hex(r, g, b)
   [r, g, b].inject('#') do |hex, n|
     hex += n.to_s(16).rjust(2, '0')
   end
 end
 
+# 16進数→10進数変換
 def to_ints(hex)
-  r = hex[1..2]
-  g = hex[3..4]
-  b = hex[5..6]
-  [r, g, b].map do |s|
-    s.hex
-  end
+  hex.scan(/\w\w/).map(&:hex)
 end
